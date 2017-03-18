2c516267217dda2f669b630edc056ea8a9ef4089
Hemantha Wijegunarathna <Hemantha Wijegunarathna>
2017-03-18 11:24:08 +0530

    add calendar to the estimation page

--- html/js/users.estimations.js
+++ html/js/users.estimations.js
@@ -1,3 +1,8 @@
-/**
- * Created by dhanushka on 3/6/2017.
- */
+$(document).ready(function ($) {
+    $('#calendar').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    });
+});
